871cb5da45368b5ca45d6a7474e5a3f46a6f7573
Kristaps Ozolins 221RDB345 <71753021+kristaps200303@users.noreply.github.com>
2024-03-11 02:43:07 +0200

    Update settings.py

--- myvoteing/myvoteing/settings.py
+++ myvoteing/myvoteing/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,6 +132,8 @@ LOGGING = {
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=os.join(BASE_DIR,'static'),
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
 LOGIN_URL = 'login'
 
 # Default primary key field type
